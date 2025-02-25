84971033055d650e4697639b10c5608b6c53daa3
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-02-25 13:48:13 -0600

    added score and imperative handle+ref

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,13 +1,32 @@
-export default function ResultModal ({ref, result, targetTime}) {
+import { useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
 
-return (
-	<dialog ref={ref} className="result-modal">
-		<h2>You {result}</h2>
+export default function ResultModal ({ref, targetTime, remainingTime, onReset}) {
+const dialog = useRef();
+
+const userLost = remainingTime <=0;
+const formattedRemaningTime = (remainingTime/1000).toFixed(2);
+const score = Math.round((1-remainingTime / (targetTime *1000)) *100);
+
+
+useImperativeHandle(ref, () => {
+	return {
+		open() {
+			dialog.current.showModal();
+		}
+	}
+});
+
+return createPortal(
+	<dialog ref={dialog} className="result-modal" onClose={onReset}>
+		{userLost && <h2>You lost</h2>}
+		{!userLost && <h2>Your score:{score}</h2>}
     <p>The target time was <strong>{targetTime} seconds.</strong></p>
-		<p>You stopped the timer with <strong>X seconds left.</strong></p>
-		<form method="dialog">
+		<p>You stopped the timer with <strong>{formattedRemaningTime} seconds left.</strong></p>
+		<form method="dialog" onSubmit={onReset}>
 			<button>Close</button>
 		</form>
-  </dialog>
+  </dialog>,
+	document.getElementById('modal')
     );
-}
+};
